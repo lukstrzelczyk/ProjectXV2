36f38bcdb01e731596cb6e92df44fe043d238388
Łukasz Strzelczyk <lukaszmiloblad@gmail.com>
2023-05-01 17:09:22 +0200

    small changes

--- ProjectXV2/Vehicle.cpp
+++ ProjectXV2/Vehicle.cpp
@@ -1,1 +1,36 @@
 #include "Vehicle.h"
+
+Vehicle::Vehicle(size_t w=0, size_t s=0, size_t ft=0, size_t f=0, std::string t="", size_t c=0) : weight{w}, speed{s}
+, max_range{ ft }, range{ f }, type{ t }, crew{ c } {
+	engine = nullptr;
+}
+
+Vehicle::~Vehicle()
+{
+	delete engine;
+}
+
+size_t Vehicle::get_weight() const
+{
+	return weight;
+}
+size_t Vehicle::get_speed()const
+{
+	return speed;
+}
+size_t Vehicle::get_range()const
+{
+	return range;
+}
+size_t Vehicle::get_max_range()const
+{
+	return max_range;
+}
+size_t Vehicle::get_crew()const
+{
+	return crew;
+}
+std::string Vehicle::get_type()const
+{
+	return type;
+}
--- ProjectXV2/Vehicle.h
+++ ProjectXV2/Vehicle.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <iostream> 
-#include <array>
 #include "Engine.h"
 #include "Diesel.h"
 #include "Functions.h"
@@ -12,20 +11,21 @@ protected:
 private:
 	size_t weight{ 1000 };
 	size_t speed{50};
-	const size_t fuel_tank{100};
-	size_t fuel{100};
+	const size_t max_range{100};
+	size_t range{100};
 	const std::string type{"Vehicle"};
 	size_t crew{ 2 };
 	//to do list of crewman nwe class crewman
 public:
-	Vehicle(size_t w, size_t s, size_t ft, size_t f, std::string t,size_t c) : weight{ w }, speed{ s }
-		, fuel_tank{ ft }, fuel{ f }, type{ t }, crew{ c } {
-		engine = new Diesel;
-	};
-	~Vehicle() {
-		delete engine;
-	}
+	Vehicle(size_t w = 0, size_t s = 0, size_t ft = 0, size_t f = 0, std::string t = "", size_t c = 0) ;
+	~Vehicle();
 	virtual void refuel() = 0;
 	virtual void drive() = 0;
+	size_t get_weight()const;
+	size_t get_speed()const;
+	size_t get_range()const;
+	size_t get_max_range()const;
+	size_t get_crew()const;
+	std::string get_type()const;
 };
 
